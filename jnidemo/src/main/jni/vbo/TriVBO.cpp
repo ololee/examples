a901e0da09dcbebcaf6b207751cd935b4d608ab3
ololee <ololeecn@163.com>
2021-11-18 23:57:15 +0800

    modify vbo error

--- jnidemo/src/main/jni/vbo/TriVBO.cpp
+++ jnidemo/src/main/jni/vbo/TriVBO.cpp
@@ -3,19 +3,19 @@
 //
 #include "../common.h"
 #include <GLES3/gl3.h>
-#include "glutils.h"
+#include "../gles/glutils.h"
 #include <GlES3/gl32.h>
 #include <GLES3/gl3ext.h>
 #include <GLES3/gl3platform.h>
 #include <jni.h>
 
-GLuint traingles_program;
-GLuint traingles_vertex_handler;
-GLuint traingles_frag_handler;
-GLuint traingles_tess_control_handler;
-GLuint traingles_tess_evaluation_handler;
+GLuint vbo_traingles_program;
+GLuint vbo_traingles_vertex_handler;
+GLuint vbo_traingles_frag_handler;
+GLuint vbo_traingles_tess_control_handler;
+GLuint vbo_traingles_tess_evaluation_handler;
 
-void init_tri() {
+void init_tri_vbo() {
     char vertexShaderText[] = "attribute  vec4 vPosition;  "
                               "void main(){  \n"
                               "gl_Position=vPosition;  \n"
@@ -47,20 +47,20 @@ void init_tri() {
 //                                      "        gl_TessCoord.z * gl_in[2].gl_Position +\n"
 //                                      "    );\n"
 //                                      "}";
-    traingles_program = CreateProgram(vertexShaderText,
+    vbo_traingles_program = CreateProgram(vertexShaderText,
                                       fragmentShaderText,
 //                                      tessControlShaderText,
 //                                      tessEvaluationShaderText,
-                                      traingles_vertex_handler,
-                                      traingles_frag_handler
+                                      vbo_traingles_vertex_handler,
+                                      vbo_traingles_frag_handler
 //                                      ,traingles_tess_control_handler,
 //                                      traingles_tess_evaluation_handler
     );
 //    glPatchParameteri(GL_PATCH_VERTICES,3);
 }
 
-void draw() {
-    if (traingles_program == GL_NONE) return;
+void draw_vbo() {
+    if (vbo_traingles_program == GL_NONE) return;
 
     glClear(GL_STENCIL_BUFFER_BIT | GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -72,13 +72,13 @@ void draw() {
     };
 //    GLushort indices[] = {0, 1, 2};
     GLfloat colors[] = {0.8f, 0.4f, 0.1f, 1.0f};
-    glUseProgram(traingles_program);
-    GLint vPos = glGetAttribLocation(traingles_program, "vPosition");
+    glUseProgram(vbo_traingles_program);
+    GLint vPos = glGetAttribLocation(vbo_traingles_program, "vPosition");
     glVertexAttribPointer(vPos, 3, GL_FLOAT,
                           GL_FALSE, 3 * sizeof(GLfloat), verticesCoords);
     glEnableVertexAttribArray(0);
 
-    GLint color_pos = glGetUniformLocation(traingles_program, "vColor");
+    GLint color_pos = glGetUniformLocation(vbo_traingles_program, "vColor");
     glUniform4fv(color_pos, 1, colors);
 //    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, indices);
 //    glPolygonMode(GL_FRONT_AND_BACK,GL_LINES);
@@ -88,20 +88,21 @@ void draw() {
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_cn_ololee_jnidemo_traingles_TrainglesRenderer_surfaceCreate(JNIEnv *env, jobject thiz) {
-init_tri();
+Java_cn_ololee_jnidemo_traingles_vbo_TriWithBufferRenderer_surfaceCreate(JNIEnv *env, jobject thiz) {
+    init_tri_vbo();
 }
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_cn_ololee_jnidemo_traingles_TrainglesRenderer_surfaceChange(JNIEnv *env, jobject thiz,
+Java_cn_ololee_jnidemo_traingles_vbo_TriWithBufferRenderer_surfaceChange(JNIEnv *env, jobject thiz,
         jint width, jint height) {
 glViewport(0, 0, width, height);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_cn_ololee_jnidemo_traingles_TrainglesRenderer_drawFrame(JNIEnv *env, jobject thiz) {
-draw();
+Java_cn_ololee_jnidemo_traingles_vbo_TriWithBufferRenderer_drawFrame(JNIEnv *env, jobject thiz) {
+    draw_vbo();
 }
+
 
